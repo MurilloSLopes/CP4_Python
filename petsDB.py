331cb2c642c7846c4867ebdc3af8e23431d130f1
murilloslopes <murillosivierolopes2310@gmail.com>
2026-08-28 18:51:20 -0300

    feat: Revisao do arquivo petsDB.py e criação de comentario!!

--- petsDB.py
+++ petsDB.py
@@ -3,6 +3,10 @@ from typing import Dict, List, Tuple
 PetKey = Tuple[str, str, int]
 PetsDB = Dict[PetKey, List[object]]
 
+# Este arquivo atua como o nosso "banco de dados" falso inicial. 
+# Quando o sistema rodar, ele vai importar essa variável (db_pets)
+# vamos trabalhar manipulando ela
+
 db_pets: PetsDB = {
     ("CC", "00001", 2024): [
         "Max",
